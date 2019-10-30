85213946b1e0d0bd85a3cd6f0cedc8465251b928
EdwardTheMaker <eduardodepolaris@gmail.com>
2019-10-29 18:39:04 -0600

    Se agrega comentario para explicar la addicion JS
    El codigo es bueno pero confuso.

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //usando el operador + para manejar las variables como numeros para evitar concatenacion de Strings.
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
